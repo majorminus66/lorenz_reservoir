fixed normalization getting avg in 100's

rservoir norm ones test input connection.py
@@ -18,9 +18,9 @@ np.random.seed(0)
 
 #why is lorenz signal passing 0?
 
-
+#58 seems to be unstable boundary
 class Reservoir:
-    def __init__(self, rsvr_size = 300, spectral_radius = 0.9, input_weight = 1):
+    def __init__(self, rsvr_size = 300, spectral_radius = 0.8, input_weight = 1):
         self.rsvr_size = rsvr_size
         
         #get spectral radius < 1
@@ -47,26 +47,42 @@ class Reservoir:
         self.Wout = np.array([])
         
 class RungeKutta:
-    def __init__(self, x0 = 1,y0 = 1,z0 = 1, h = 0.01, T = 300, ttsplit = 5000):
+    def __init__(self, x0 = 2,y0 = 2,z0 = 30, h = 0.01, T = 300, ttsplit = 5000, noise_scaling = 0.0):
         u_arr = rungekutta(x0,y0,z0,h,T)[:, ::5]
         
-        for i in range(u_arr[:,0].size):
-            u_arr[i] = (u_arr[i] - np.mean(u_arr[i]))/np.std(u_arr[i])
+        #print(np.std(u_arr[0]))
+        #print(np.std(u_arr[1]))
+        #print(np.std(u_arr[2]))
         
+        u_arr[0] = (u_arr[0] - 0)/7.929788629895004
+        u_arr[1] = (u_arr[1] - 0)/8.9932616136662
+        u_arr[2] = (u_arr[2] - 23.596294463016896)/8.575917849311919
         
         self.u_arr_train = u_arr[:, :ttsplit+1]
         #size 5001
+        
+        #noisy training array
+        #switch to gaussian 
+        noise = (np.random.rand(self.u_arr_train[:,0].size, self.u_arr_train[0,:].size)-0.5)*noise_scaling
+        self.u_arr_train_noise = self.u_arr_train + noise
+        
+        #plt.plot(self.u_arr_train_noise[0, :500])
         
         #u[5000], the 5001st element, is the last in u_arr_train and the first in u_arr_test
         self.u_arr_test = u_arr[:, ttsplit:]
         #size 1001
     
 #takes a reservoir object res along with initial conditions
-def getX(res, rk,x0 = 1,y0 = 1,z0 = 1):
+def getX(res, rk,x0 = 1,y0 = 1,z0 = 1, noise = False):
+    
+    if noise:
+        u_training = rk.u_arr_train_noise
+    else:
+        u_training = rk.u_arr_train
     
     #loops through every timestep
-    for i in range(0, rk.u_arr_train[0].size):
-        u = np.append(1, rk.u_arr_train[:,i]).reshape(4,1)
+    for i in range(0, u_training[0].size):
+        u = np.append(1, u_training[:,i]).reshape(4,1)
         
         x = res.X[:,i].reshape(res.rsvr_size,1)
         x_update = np.tanh(np.add(res.Win.dot(u), res.W.dot(x)))
@@ -81,11 +97,11 @@ def trainRRM(res, rk):
     alph = 10**-4
     #rrm = Ridge(alpha = alph, solver = 'cholesky')
     
-    Y_train = rk.u_arr_train[:, 201:]
+    Y_train = rk.u_arr_train[:, 301:]
 
     
-    X = getX(res, rk)[:, 201:(res.X[0].size - 1)]
-    X_train = np.concatenate((np.ones((1, 4800)), X, rk.u_arr_train[:, 200:(rk.u_arr_train[0].size - 1)]), axis = 0)
+    X = getX(res, rk, noise = True)[:, 301:(res.X[0].size - 1)]
+    X_train = np.concatenate((np.ones((1, 4700)), X, rk.u_arr_train[:, 300:(rk.u_arr_train[0].size - 1)]), axis = 0)
     #X_train = np.copy(X)
         
     idenmat = np.identity(res.rsvr_size+4)*alph
@@ -126,12 +142,12 @@ def predict(res, x0 = 0, y0 = 0, z0 = 0, steps = 1000):
 
     return Y
 
-def test(res, num_tests = 10, rkTime = 105, split = 2000, showPlots = True):
+def test(res, num_tests = 10, rkTime = 150, split = 2000, showPlots = True):
     valid_time = np.array([])
     ICerror = np.array([])
     for i in range(num_tests):
         ic = np.random.rand(3)*2-1
-        rktest = RungeKutta(x0 = ic[0], y0 = ic[1], z0 = ic[2], T = rkTime, ttsplit = split)
+        rktest = RungeKutta(x0 = ic[0], y0 = ic[1], z0 = 30*ic[2], T = rkTime, ttsplit = split)
         res.X = (np.zeros((res.rsvr_size, split+2))*2 - 1)
         
         #sets res.X
@@ -140,7 +156,7 @@ def test(res, num_tests = 10, rkTime = 105, split = 2000, showPlots = True):
         pred = predict(res, x0 = rktest.u_arr_test[0,0], y0 = rktest.u_arr_test[1,0], z0 = rktest.u_arr_test[2,0], steps = (rkTime*20-split))
         
         for j in range(0, pred[0].size):
-            if np.abs(pred[0, j] - rktest.u_arr_test[0, j]) > 0.5:
+            if np.abs(pred[0, j] - rktest.u_arr_test[0, j]) > 1.5:
                 valid_time = np.append(valid_time, j)
                 print("Test " + str(i) + " valid time: " + str(j))
                 break